9af8dfa021dacd5d65ff511e5a39c1636348059c
Yahya Fahmy <yahyafahmy@Mac.cust.communityfibre.co.uk>
2025-11-04 18:02:08 +0000

    removed bombs/discard, new weights

--- player.py
+++ player.py
@@ -6,124 +6,130 @@ import math
 BEST_WEIGHTS = {
     "w1": [
         [
-            -1.1835961451451742,
-            1.202704810893144,
-            -1.6356226519354693,
-            -5.930551071985017,
-            -0.4355109318595083,
-            3.819259626404543,
-            1.636868656155593,
-            -3.591994174357847,
-            0.6700058757050474,
-            0.34452461329875905
+            -0.1927062174432148,
+            -1.8904515672692002,
+            3.854917019406205,
+            -0.3692542925261456,
+            4.398636433050703,
+            1.813256170879606
         ],
         [
-            -4.352970054092909,
-            3.3005937589441494,
-            0.5820711701508127,
-            1.5554272327283716,
-            -0.35566991950057747,
-            -0.7977752187289501,
-            -2.2949964740317252,
-            -2.577126860390476,
-            4.682540535586572,
-            3.0455671894428087
+            -1.6411909017881579,
+            3.44766244961282,
+            0.5565415547440347,
+            0.1998394375500775,
+            1.5108056015301843,
+            1.9289656648563307
         ],
         [
-            2.1261471529021905,
-            -1.5461159550752952,
-            -0.6844859964948461,
-            -2.0919528047848024,
-            0.1575038076698171,
-            -1.081949983800732,
-            0.9060370382770703,
-            0.13915700404127285,
-            3.4063452612600846,
-            -0.5987811958760434
+            0.6865079594433822,
+            0.4021603775944168,
+            0.21249426728618814,
+            5.209655190647702,
+            3.3526950375578197,
+            -0.6405203702747556
         ],
         [
-            2.396569811897969,
-            2.4443339441327208,
-            2.0990550821817466,
-            0.19028597367624023,
-            -0.5029225082943397,
-            -3.7227849905915624,
-            1.442422720947865,
-            -2.506298602788796,
-            -1.4134721257127416,
-            -2.941546598729561
+            -0.18516251350800172,
+            2.281472179486085,
+            0.175179724309243,
+            0.32120126193601173,
+            -0.15305529321542732,
+            0.49422206904756727
         ],
         [
-            0.8271602756477602,
-            -2.8288466818556657,
-            -0.8136612955393888,
-            -6.750884307856305,
-            -1.8672543262741117,
-            0.23061840106965104,
-            -1.6911805693411845,
-            2.7761497143388056,
-            -0.41487562028568115,
-            -1.0685725219866824
+            0.5637355778836036,
+            -0.29696511521934055,
+            -1.1254929629838517,
+            1.2408765835490283,
+            4.061394344655122,
+            0.5468292648146064
         ],
         [
-            -0.3483971744570251,
-            -3.191325965468982,
-            -2.272904694187633,
-            -0.8888183793198599,
-            1.8006266784957472,
-            -2.4026369157659415,
-            0.35709067516667714,
-            -1.3834956805313183,
-            1.3985913465241482,
-            1.4075079023669523
+            -2.416529793326332,
+            -0.2748876188592815,
+            0.06856766303082051,
+            2.600081762720423,
+            0.9394715482340735,
+            3.0045136175937857
+        ],
+        [
+            0.02544398284597349,
+            -0.26486298983913237,
+            1.1344917125618994,
+            0.4875675797223741,
+            2.6681373472025838,
+            -0.4000585631828416
         ]
     ],
     "b1": [
-        -1.8743572713759347,
-        -0.5712287690383885,
-        -1.7016494616689437,
-        0.359735674750869,
-        -2.724524436477339,
-        -0.37561421600188094,
-        3.091303271447589,
-        -2.1778156021849773,
-        -1.4715657555006179,
-        -0.5877844887167009
+        -1.7102244699710405,
+        2.1762622377706586,
+        -1.533427543507537,
+        0.3644381075506623,
+        -0.3548203165863567,
+        1.9419284599477047
     ],
     "w2": [
         [
-            -0.3845185719535079
+            0.3727536554689621,
+            -0.877485849533826,
+            0.7550842741884309,
+            2.725326944314095
         ],
         [
-            -0.6119317821459572
+            -1.1059422715407012,
+            0.10441837320248659,
+            -3.015296536764048,
+            0.8179927001067527
         ],
         [
-            -0.39332601266362477
+            2.315801511790079,
+            3.685800364208931,
+            1.4244090418070328,
+            -1.5581888666690002
         ],
         [
-            2.2441040027704884
+            -2.932015375126067,
+            -0.9449908579589205,
+            0.26509416172712197,
+            -1.9759265464930238
         ],
         [
-            0.026903704194055966
+            -2.2697626042098977,
+            1.166254347997198,
+            1.5052533321023756,
+            2.4663942284300004
         ],
         [
-            -2.3371492081888725
-        ],
-        [
-            -0.6645523301291176
-        ],
-        [
-            1.8552026225275235
-        ],
-        [
-            -1.0894794955257425
-        ],
-        [
-            -0.8364735100732846
+            -2.241610146305781,
+            -0.4151450052478951,
+            1.2892229939671185,
+            6.049852833480946
         ]
     ],
     "b2": [
-        -3.900883693626965
+        0.3664655395280376,
+        -2.256314264314429,
+        0.6579677676498774,
+        -2.029722775798487
+    ],
+    "w3": [
+        [
+            0.8521590985734687
+        ],
+        [
+            -2.061004553639114
+        ],
+        [
+            -1.633228095535831
+        ],
+        [
+            -0.16820764979440328
+        ]
+    ],
+    "b3": [
+        1.1981144292187502
     ]
 }
 
@@ -134,8 +140,8 @@ class Player:
 
 
 class AIPlayer(Player):
-    
-    HIDDEN_NEURONS = 10
+    HIDDEN_NEURONS_1 = 6
+    HIDDEN_NEURONS_2 = 4
 
     def __init__(self, seed=None, weights=None):
         self.move_queue = []
@@ -147,14 +153,18 @@ class AIPlayer(Player):
             self.b1 = weights['b1']
             self.w2 = weights['w2']
             self.b2 = weights['b2']
+            self.w3 = weights['w3']
+            self.b3 = weights['b3']
         else:
             self.w1 = BEST_WEIGHTS['w1']
             self.b1 = BEST_WEIGHTS['b1']
             self.w2 = BEST_WEIGHTS['w2']
             self.b2 = BEST_WEIGHTS['b2']
+            self.w3 = BEST_WEIGHTS['w3']
+            self.b3 = BEST_WEIGHTS['b3']
 
     def get_weights(self):
-        return {'w1': self.w1, 'b1': self.b1, 'w2': self.w2, 'b2': self.b2}
+        return {'w1': self.w1, 'b1': self.b1, 'w2': self.w2, 'b2': self.b2, 'w3': self.w3, 'b3': self.b3}
 
     def print_board(self, board):
         print("--------")
@@ -183,33 +193,10 @@ class AIPlayer(Player):
                 first_board.land_block()
                 first_moves.append((first_board, (r1, x1)))
 
-        # add discard as a move if available
-        if board.discards_remaining > 0:
-            discard_board = board.clone()
-            discard_board.discards_remaining -= 1
-            discard_board.place_next_block()  # disc current, place next
-            first_moves.append((discard_board, 'DISCARD'))
-
-        # add bomb as move if available
-        if board.bombs_remaining > 0 and board.next is not None and board.next.shape != 'B':
-            bomb_board = board.clone()
-            bomb_board.bombs_remaining -= 1
-            # Set the next piece to a bomb
-            bomb_board.next = Block(Shape.B)
-            first_moves.append((bomb_board, 'BOMB'))
-
-        #return if next block lookahead is not being used
-        moves_2d = []
-        actions_2d = []
-        for first_board, first_action in first_moves:
-            moves_2d.append([first_board])
-            actions_2d.append([(first_action, None)])
-        return moves_2d, actions_2d
-
         # next block lookahead
         first_scores = [self.get_score(b, board) for b, _ in first_moves]
-        if len(first_scores) > 5:
-            top_indices = sorted(range(len(first_scores)), key=lambda i: first_scores[i], reverse=True)[:4]
+        if len(first_scores) > 3:
+            top_indices = sorted(range(len(first_scores)), key=lambda i: first_scores[i], reverse=True)[:3]
         else:
             top_indices = list(range(len(first_scores)))
 
@@ -219,48 +206,24 @@ class AIPlayer(Player):
             first_board, first_action = first_moves[idx]
             row_moves = []
             row_actions = []
-            if first_action == 'DISCARD':
-                row_moves.append(first_board)
-                row_actions.append(('DISCARD', None))
-            elif first_action == 'BOMB':
-                #all possible placements for the bomb piece
-                if first_board.falling is not None:
-                    for r2 in range(4):
-                        for x2 in range(10):
-                            second_board = first_board.clone()
-                            for _ in range(r2):
-                                second_board.falling.rotate(Rotation.Clockwise, second_board)
-                            dx2 = x2 - second_board.falling.left
-                            if dx2 < 0:
-                                second_board.falling.move(Direction.Left, second_board, -dx2)
-                            elif dx2 > 0:
-                                second_board.falling.move(Direction.Right, second_board, dx2)
-                            second_board.falling.move(Direction.Drop, second_board)
-                            second_board.land_block()
-                            row_moves.append(second_board)
-                            row_actions.append(('BOMB', (r2, x2)))
-                else:
-                    row_moves.append(first_board)
-                    row_actions.append(('BOMB', None))
+            if first_board.falling is not None:
+                for r2 in range(4):
+                    for x2 in range(10):
+                        second_board = first_board.clone()
+                        for _ in range(r2):
+                            second_board.falling.rotate(Rotation.Clockwise, second_board)
+                        dx2 = x2 - second_board.falling.left
+                        if dx2 < 0:
+                            second_board.falling.move(Direction.Left, second_board, -dx2)
+                        elif dx2 > 0:
+                            second_board.falling.move(Direction.Right, second_board, dx2)
+                        second_board.falling.move(Direction.Drop, second_board)
+                        second_board.land_block()
+                        row_moves.append(second_board)
+                        row_actions.append((first_action, (r2, x2)))
             else:
-                if first_board.falling is not None:
-                    for r2 in range(4):
-                        for x2 in range(10):
-                            second_board = first_board.clone()
-                            for _ in range(r2):
-                                second_board.falling.rotate(Rotation.Clockwise, second_board)
-                            dx2 = x2 - second_board.falling.left
-                            if dx2 < 0:
-                                second_board.falling.move(Direction.Left, second_board, -dx2)
-                            elif dx2 > 0:
-                                second_board.falling.move(Direction.Right, second_board, dx2)
-                            second_board.falling.move(Direction.Drop, second_board)
-                            second_board.land_block()
-                            row_moves.append(second_board)
-                            row_actions.append((first_action, (r2, x2)))
-                else:
-                    row_moves.append(first_board)
-                    row_actions.append((first_action, None))
+                row_moves.append(first_board)
+                row_actions.append((first_action, None))
             moves_2d.append(row_moves)
             actions_2d.append(row_actions)
         return moves_2d, actions_2d
@@ -290,6 +253,8 @@ class AIPlayer(Player):
             bump += abs(heights[i + 1] - heights[i])
 
         lines_cleared = (len(board.cells) + 4 - len(move.cells)) // move.width if move.width > 0 else 0
+        
+        aggregate_height = sum(heights)
 
         # normalising features
         norm_holes = holes / 50.0
@@ -299,27 +264,33 @@ class AIPlayer(Player):
         norm_height_var = height_var / 24.0
         norm_lines_cleared = lines_cleared / 4.0
         norm_blocks_placed = self.block_num / 400.0
-
-        return (norm_holes, norm_bump, norm_max_height, norm_height_var, norm_lines_cleared, norm_blocks_placed)
+        norm_aggregate_height = aggregate_height / 240.0  
+        
+        return (norm_holes, norm_bump, norm_max_height, norm_height_var, norm_lines_cleared, norm_blocks_placed, norm_aggregate_height)
     
     def sgmoid(self, x):
         return 1 / (1 + math.exp(-x))
 
     def fp(self, features):
-        hidden_inputs = [0] * self.HIDDEN_NEURONS
-        for i in range(self.HIDDEN_NEURONS):  
-            for j in range(6): #each feature
-                hidden_inputs[i] += features[j] * self.w1[j][i]
-            hidden_inputs[i] += self.b1[i]
-
-        # activation
-        hidden_outputs = [self.sgmoid(x) for x in hidden_inputs]
-
+        # first hidden layer
+        hidden1 = [0] * self.HIDDEN_NEURONS_1
+        for i in range(self.HIDDEN_NEURONS_1):
+            for j in range(7):  
+                hidden1[i] += features[j] * self.w1[j][i]
+            hidden1[i] += self.b1[i]
+        hidden1 = [self.sgmoid(x) for x in hidden1]
+        # second hidden layer
+        hidden2 = [0] * self.HIDDEN_NEURONS_2
+        for i in range(self.HIDDEN_NEURONS_2):
+            for j in range(self.HIDDEN_NEURONS_1):
+                hidden2[i] += hidden1[j] * self.w2[j][i]
+            hidden2[i] += self.b2[i]
+        hidden2 = [self.sgmoid(x) for x in hidden2]
+        # Output
         output = 0
-        for i in range(self.HIDDEN_NEURONS):  
-            output += hidden_outputs[i] * self.w2[i][0]
-        output += self.b2[0]
-
+        for i in range(self.HIDDEN_NEURONS_2):
+            output += hidden2[i] * self.w3[i][0]
+        output += self.b3[0]
         return output
 
     def get_score(self, move, board):
@@ -348,19 +319,10 @@ class AIPlayer(Player):
             best_action = None
             for i, row in enumerate(scores_2d):
                 for j, score in enumerate(row):
-                    next = actions_2d[i][j][0]
-                    if next == 'BOMB':
-                        score /= 0.9 #decrease frequency of bomb uses
-                    if next == 'DISCARD':
-                        score /= 0.99 #decrease frequency of discard uses
                     if (best_score is None) or (score > best_score):
                         best_score = score
-                        best_action = next
+                        best_action = actions_2d[i][j][0]
             self.scorelist.append(best_score)
-            if best_action == 'DISCARD':
-                return Action.Discard
-            if best_action == 'BOMB':
-                return Action.Bomb
             rotations, target_x = best_action
             for _ in range(rotations):
                 self.move_queue.append(Rotation.Clockwise)
